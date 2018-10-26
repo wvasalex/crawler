2bd861545796b5bc6b2171e8013d188a808a1add
vas.alex1609@gmail.com <vas.alex1609@gmail.com>
2018-10-26 17:26:45 +0300

    first commit

---
+++ crawler.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"os"
+	"strings"
+	"sync"
+)
+
+func _check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func parseUrl(url string) {
+	fmt.Println("request: " + url)
+	doc, err := goquery.NewDocument(url)
+	_check(err)
+
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		link, _ := s.Attr("href")
+		fmt.Println(link)
+	})
+}
+
+func isXML(path string) bool {
+	return strings.HasSuffix(path, ".xml")
+}
+
+func localDOM(path string) (doc *goquery.Document, err error) {
+	var f *os.File
+	f, err = os.Open(path)
+	defer f.Close()
+	return goquery.NewDocumentFromReader(f)
+}
+
+func parseSitemap(link string, local bool, accumulator []string) []string {
+	var doc *goquery.Document
+	var err error
+
+	if local {
+		doc, err = localDOM(link)
+	} else {
+		doc, err = goquery.NewDocument(link)
+	}
+	_check(err)
+
+	doc.Find("loc").Each(func(i int, node *goquery.Selection) {
+		href := node.Text()
+		if isXML(href) {
+			parseSitemap(href, false, accumulator)
+		} else {
+			parseHTML(href)
+			//accumulator = append(accumulator, href)
+		}
+	})
+
+	return accumulator
+}
+
+var i bool = false
+
+func parseHTML(href string) {
+	if i {
+		return
+	}
+
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	for _, url := range os.Args[1:] {
+		wg.Add(1)
+
+		go func(url string) {
+			defer wg.Done()
+			parseUrl(url)
+		}(url)
+	}
+
+	wg.Wait()
+
+	var products []string
+	products = parseSitemap("auchan/sitemap_index.xml", true, products)
+	fmt.Println(products)
+}
