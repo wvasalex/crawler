ca068cb56e1ccad82e6cd3aaa65d2da887e243f0
vas.alex1609@gmail.com <vas.alex1609@gmail.com>
2018-10-29 16:15:39 +0300

    crawler

--- crawler.go
+++ crawler.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"os"
+	"regexp"
 	"strings"
-	"sync"
 )
+
+type LineReaderHandler func(string)
 
 func _check(err error) {
 	if err != nil {
@@ -44,38 +47,120 @@ func localDOM(path string) (doc *goquery.Document, err error) {
 	return goquery.NewDocumentFromReader(f)
 }
 
-func parseSitemap(link string, local bool, accumulator []string) []string {
+func writeLines(lines []string, path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintln(w, line)
+	}
+	return w.Flush()
+}
+
+func readLines(path string, handler LineReaderHandler) error {
+	f, err := os.OpenFile(path, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		handler(sc.Text())
+	}
+	return sc.Err()
+}
+
+func parseSitemap(link string, local bool) []string {
+	var accumulator []string
+
 	doc, err := getDOM(link, local)
 	_check(err)
 
 	doc.Find("loc").Each(func(i int, node *goquery.Selection) {
 		href := node.Text()
 		if isXML(href) {
-			parseSitemap(href, false, accumulator)
+			accumulator = append(accumulator, parseSitemap(href, false)...)
 		} else {
-			parseHTML(href)
-			//accumulator = append(accumulator, href)
+			accumulator = append(accumulator, href)
+			//parseHTML(href)
 		}
+
 	})
 
 	return accumulator
 }
 
-var i bool = false
+type PropReader map[string]string
+type PropCollection map[string]string
 
 func parseHTML(link string) {
-	if i {
-		return
-	}
-	i = true
-
 	doc, err := getDOM(link, false)
 	_check(err)
-	fmt.Printf("Title = %s, src = %s", doc.Find("h1").Text(), link)
+
+	reader := make(map[string]PropReader)
+	reader["title"] = PropReader{"selector": "h1", "prop": "Text"}
+	reader["url"] = PropReader{"selector": "meta[property='og:url']", "prop": "Attr", "Attr": "content"}
+	reader["category"] = PropReader{"selector": "li.breadcrumbs__item", "prop": "Text"}
+	reader["price"] = PropReader{"selector": "div.current-price", "filter": "Last", "prop": "Text"}
+	reader["description"] = PropReader{"selector": "ul.prcard__feat-list", "filter": "Last", "prop": "Text"}
+	reader["code"] = PropReader{"useValue": "description", "re": "Артикул:/(?P<code>\\w+)", "prop": "Re"}
+
+	result := make(PropCollection)
+	for name, prop := range reader {
+		result[name] = readProp(doc, prop, result)
+	}
+
+	fmt.Println(result["code"])
+}
+
+func getNode(doc *goquery.Document, desc PropReader) *goquery.Selection {
+	if desc["selector"] == "" {
+		return nil
+	}
+
+	node := doc.Find(desc["selector"])
+
+	switch desc["filter"] {
+	case "Last":
+		node = node.Last()
+
+	case "First":
+		node = node.First()
+	}
+
+	return node
+}
+
+func readProp(doc *goquery.Document, desc PropReader, result PropCollection) string {
+	node := getNode(doc, desc)
+	var value string = ""
+
+	switch desc["prop"] {
+	case "Text":
+		value = node.Text()
+
+	case "Attr":
+		attr, _ := node.Attr(desc["Attr"])
+		value = attr
+
+	case "Re":
+		r := regexp.MustCompile(desc["re"])
+		value = r.FindStringSubmatch(result[desc["useValue"]])[1]
+	}
+
+	r := regexp.MustCompile("\n+")
+	value = r.ReplaceAllString(strings.TrimSpace(value), "/")
+
+	return value
 }
 
 func main() {
-	var wg sync.WaitGroup
+	/*var wg sync.WaitGroup
 
 	for _, url := range os.Args[1:] {
 		wg.Add(1)
@@ -86,9 +171,19 @@ func main() {
 		}(url)
 	}
 
-	wg.Wait()
+	wg.Wait()*/
 
-	var products []string
-	products = parseSitemap("auchan/sitemap_index.xml", true, products)
+	/*var products []string
+	products = parseSitemap("auchan/sitemap_index.xml", true)
 	fmt.Println(products)
+
+	writeLines(products, "products.txt")*/
+
+	/*var products []string
+	readLines("products.txt", func(line string) {
+		fmt.Printf("Product = %s\n", line)
+		products = append(products, line)
+	})*/
+
+	parseHTML("https://www.auchan.ru/pokupki/cosmia-kr-lica-i-tel-uvlazh-50m.html")
 }
